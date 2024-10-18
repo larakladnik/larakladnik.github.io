c9957d556f2677984f98d8ca6b41ff79346b104c
lara_kladnik <lara.kladnik@gmail.com>
2024-10-18 14:35:50 +0200

    novo

---
+++ MY website/File.js
@@ -0,0 +1,8 @@
+function openModal(src) {
+    document.getElementById("myModal").style.display = "block";
+    document.getElementById("modalImage").src = src;
+}
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
